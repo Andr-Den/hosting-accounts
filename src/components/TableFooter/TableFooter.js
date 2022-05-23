b7b2f4b35ab9961edbff7b70407c8ffce164b95a
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-05-23 17:41:05 +0500

    Add component TableFooter with deleting items

---
+++ src/components/TableFooter/TableFooter.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+import "./TableFooter.css"
+
+import cancel from "../../images/cancel.png"
+
+const domainsList = [
+  {
+    title: 'shoky-yoku.ru',
+    description: 'shoky-yoku.ru (alias)'
+  },
+  {
+    title: 'srv164042.hoster-test.ru',
+    description: 'srv164042.hoster-test.ru (alias)'
+  },
+  {
+    title: 'сёкуёкую.рф',
+    description: 'www.сёкуёкую.рф'
+  },
+]
+
+function TableFooter() {
+  const [domains, setDomains] = React.useState(JSON.parse(localStorage.getItem('domains')));
+
+  React.useEffect(() => {
+    localStorage.setItem('domains', JSON.stringify(domainsList))
+  })
+
+  function handleDelete(index) {
+    domains.splice(index, 1)
+    localStorage.setItem('domains', JSON.stringify(domains))
+    setDomains(JSON.parse(localStorage.getItem('domains')))
+  }
+
+  return (
+    <tfoot>
+      <tr>
+        <td className="table-footer"><a href="*" className="table-footer__link">Привязанные домены</a></td>
+        <td colSpan="6">
+          <ul className="table-footer__list">
+            {domains.map(({title, description}) => (
+              <li>
+                <a href="*" className="table-footer__link table-footer__link_bold">{title}</a>
+                <div className="table-footer__item">
+                  <span>{description}</span>
+                  <button className="table-footer__button" onClick={handleDelete}><img src={cancel} alt="" className="table-footer__image"/></button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </td>
+      </tr>
+    </tfoot>
+  )
+}
+
+export default TableFooter
