39157241aba93d8b39387e563758010b97b8db8c
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-05-23 19:35:55 +0500

    Add icon to Table

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -5,15 +5,20 @@ import TableFooter from "../TableFooter/TableFooter";
 
 import './Table.css'
 
+import question from "../../images/question.png"
+
 function Table() {
   return (
     <div className="table">
-      <h1  className="table__heading">АККАУНТЫ ХОСТИНГА</h1>
-    <table cellSpacing="0" className="table__table">
-      <TableHeader />
-      <TableBody /> 
-      <TableFooter />
-    </table>
+      <div className="table__up">
+        <h1 className="table__heading">АККАУНТЫ ХОСТИНГА</h1>
+        <img alt="" src={question} className="table__question"/>
+      </div>
+      <table cellSpacing="0" className="table__table">
+        <TableHeader />
+        <TableBody /> 
+        <TableFooter />
+      </table>
     </div>
   )
 }
